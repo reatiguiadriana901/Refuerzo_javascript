58a21706bf34e51a0d7c6276b80b8305ec516572
reatiguiadriana901 <reatiguiadriana901@gmail.com>
2026-06-08 21:48:11 -0500

    feat: :sparkles: creacion de array con objetos con datos de usuario

---
+++ ej6.js
@@ -0,0 +1,27 @@
+const usuarios = [
+    {
+        nombre: "Adriana",
+        rol: "vulcanologa",
+        biografia: "Explorando la fuerza de la Tierra entre líneas de código y volcanes activos."
+    },
+    {
+        nombre: "Daniela",
+        rol: "Geofísica",
+        biografia: "Descifrando los secretos ocultos del planeta a través de la física y la geología."
+    }, 
+    {
+        nombre: "Shairet",
+        rol: "Geomecánica",
+        biografia: "Analizando la resistencia de las rocas para entender cómo se sostiene el mundo."
+    }, 
+    {
+        nombre: "Luisa",
+        rol: "Paleoambientalista",
+        biografia: "Viajando al pasado a través de la geología para entender el futuro de nuestro clima."
+    },
+    {
+        nombre: "laura",
+        rol: "Hidrogeologa",
+        biografia: "Siguiendo la ruta del agua subterránea para proteger el recurso más vital del planeta."
+    },
+];
